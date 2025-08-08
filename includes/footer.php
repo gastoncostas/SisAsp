efea747152aa868bda5f02a38da966439b274f40
Gastón Costas <gncostas@gmail.com>
2025-08-08 03:29:15 -0300

    eliminar repetidos.

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <link rel="stylesheet" href="/assets/css/footer.css">
-    <div class="footer-container">
-        <p>&copy; <?php echo date('Y'); ?> Sistema de Gestión de la ESyA - Policía de Tucumán</p>
-    </div>
-</footer>
